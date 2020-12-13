d0e21fba8f213a9cbabcfa3587ffc0fc4d7ad284
Dan Ivovich <divovich@gmail.com>
2020-12-13 08:39:00 -0500

    Day 7 part 1 done

--- day7/part1.exs
+++ day7/part1.exs
@@ -1,6 +1,6 @@
 defmodule Part1 do
   def run do
-    {:ok, contents} = File.read("sample.txt")
+    {:ok, contents} = File.read("input.txt")
 
     rules =
       contents
@@ -11,26 +11,9 @@ defmodule Part1 do
         |> Map.merge(map)
       end)
 
-    collapsed_rules =
-      rules
-      |> Map.keys()
-      |> Enum.reduce(%{}, fn container, map ->
-        {:ok, content} = Map.fetch(rules, container)
-
-        collapsed =
-          Enum.reduce(content, [], fn bag, sum ->
-            sum ++ collapse_rule(bag, rules)
-          end)
-          |> List.flatten()
-          |> Enum.reject(fn x -> x == nil end)
-
-        Map.put(map, container, collapsed)
-      end)
-
-    Enum.filter(Map.values(collapsed_rules), fn content ->
-      Enum.any?(content, fn x -> x == "shiny gold" end)
+    Enum.count(Map.keys(rules), fn bag ->
+      can_hold(rules[bag], rules)
     end)
-    |> length
     |> IO.inspect()
   end
 
@@ -55,12 +38,20 @@ defmodule Part1 do
     Map.put(%{}, container, clean_content)
   end
 
-  defp collapse_rule(nil, _rules) do
-    []
+  defp can_hold([nil], _rules) do
+    false
   end
 
-  defp collapse_rule(bag, rules) do
-    [bag, collapse_rule(rules[bag], rules)]
+  defp can_hold([], _rules) do
+    false
+  end
+
+  defp can_hold(["shiny gold" | _bags], _rules) do
+    true
+  end
+
+  defp can_hold([bag | bags], rules) do
+    can_hold(bags, rules) || can_hold(rules[bag], rules)
   end
 end
 
